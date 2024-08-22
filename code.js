7b30338b24bb2d894dae3ea67b9593cb19fdad49
Chad <cbergman@figma.com>
2024-08-22 14:25:38 -0700

    Update for string type

--- code.js
+++ code.js
@@ -190,6 +190,15 @@ function traverseToken({
         key,
         object.$value
       );
+    } else if (type === "string") {
+      tokens[key] = tokens[key] = createToken(
+        variableMap,
+        collection,
+        modeId,
+        "STRING",
+        key,
+        object.$value
+      );
     } else {
       console.log("unsupported type", type, object);
     }
@@ -228,13 +237,28 @@ function processCollection({ name, modes, variableIds }) {
       const { name, resolvedType, valuesByMode } =
         figma.variables.getVariableById(variableId);
       const value = valuesByMode[mode.modeId];
-      if (value !== undefined && ["COLOR", "FLOAT"].includes(resolvedType)) {
+      if (value !== undefined && ["COLOR", "FLOAT", "STRING"].includes(resolvedType)) {
         let obj = file.body;
         name.split("/").forEach((groupName) => {
           obj[groupName] = obj[groupName] || {};
           obj = obj[groupName];
         });
-        obj.$type = resolvedType === "COLOR" ? "color" : "number";
+        let variableType = "";
+        switch(resolvedType) {
+          case "COLOR":
+            variableType = "color";
+            break;
+          case "FLOAT":
+            variableType = "number";
+            break;
+          case "STRING":
+            variableType = "string";
+            break;
+          default:
+            variableType = "unknown";
+            break;
+        }
+        obj.$type = variableType;
         if (value.type === "VARIABLE_ALIAS") {
           obj.$value = `{${figma.variables
             .getVariableById(value.id)
